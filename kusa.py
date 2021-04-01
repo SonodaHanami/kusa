0c0faec70a733bb920f4e58a1ab0bdbd0750cca8
SonodaHanami <umichangadaisuki@gmail.com>
2021-04-01 19:29:38 +0800

    adapt to yobot

--- kusa.py
+++ kusa.py
@@ -1,10 +1,22 @@
-class Kusa:
-    def __init__(self):
-        pass
+from aiocqhttp.api import Api
 
-    async def execute_async(self, message):
+class Kusa:
+    Passive = True
+    Active = False
+    Request = False
+
+    def __init__(self, bot_api: Api, **kwargs):
+        self.api = bot_api
+
+    def match(self, msg):
+        return 1
+
+    async def execute_async(self, func_num, message):
         msg = message['raw_message'].strip()
         group = str(message.get('group_id', ''))
         user = str(message.get('user_id', ''))
 
-        return '草'
+        replys = []
+
+
+        return '\n'.join(replys) if replys else None
