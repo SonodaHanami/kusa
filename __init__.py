18b717de65d128b6607c2d08e457879cc7da847a
SonodaHanami <umichangadaisuki@gmail.com>
2021-04-02 20:49:33 +0800

    add config

--- __init__.py
+++ __init__.py
@@ -1,5 +1,20 @@
+import os
+import json
 from .kusa import Kusa
 
 __all__ = [
     'Kusa',
 ]
+
+default_config = {
+    'ADMIN': 0,
+    'BOT': 0,
+}
+
+config_path = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    'config.json'
+)
+if not os.path.exists(config_path):
+    with open(config_path, 'w') as f:
+        json.dump(default_config, f, indent=4, ensure_ascii=False)
