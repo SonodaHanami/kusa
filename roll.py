6a5084e69a3a36981fdf10a699c22b6b6ed28ccd
SonodaHanami <umichangadaisuki@gmail.com>
2021-04-08 16:48:56 +0800

    feat(roll): more new pattern

--- roll.py
+++ roll.py
@@ -2,6 +2,7 @@ import random
 import re
 from random import randint as ri
 
+DEFAULT_PLANE = 6
 MAX_DICE = 10
 MAX_PLANE = 20
 
@@ -23,21 +24,30 @@ class Roll:
             if not msg:
                 return '{}'.format(ri(1, 100))
             try:
-                prm = re.findall('(\d+)d(\d+)', msg)
+                prm = re.findall('(\d+)?d(\d+)?([\+\-]\d+)?', msg)
                 for p in prm:
-                    if int(p[1]) > MAX_PLANE:
+                    dice =  int(p[0]) if p[0] else 1
+                    plane = int(p[1]) if p[1] else DEFAULT_PLANE
+                    delta = int(p[2]) if p[2] else 0
+                    if plane <= 0:
+                        return '¿'
+                    if plane > MAX_PLANE:
                         return '这么多面，建议玩球'
-                    for i in range(int(p[0])):
-                        replys.append('{}'.format(ri(1, int(p[1]))))
+                    for i in range(dice):
+                        replys.append('{}'.format(ri(1, plane) + delta))
                 if replys:
                     if len(replys) > MAX_DICE:
                         return '唔得，骰子不够叻'
                     return ' '.join(replys)
-                prm = re.match('(\d+) +(\d+)', msg)
+
+                prm = re.match('([\+\-]?\d+) +([\+\-]?\d+)', msg)
                 if prm:
-                    return '{}'.format(ri(int(prm[1]), int(prm[2])))
+                    l, r = min(int(prm[1]), int(prm[2])), max(int(prm[1]), int(prm[2]))
+                    return '{}'.format(ri(l, r))
                 prm = re.match('(\d+)', msg)
                 if prm:
+                    if int(prm[1]) <= 0:
+                        return 0
                     return '{}'.format(ri(1, int(prm[1])))
             except Exception as e:
                 return 'Roll error: {}'.format(e)
